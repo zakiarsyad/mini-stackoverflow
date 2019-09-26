0b8d4c6dcaf9a8ee88624d0ee63b0afccd2d4459
zakiarsyad <ozikarsyad@gmail.com>
2019-09-26 16:26:28 +0700

    server is ready

---
+++ server/configs/email.js
@@ -0,0 +1,39 @@
+let nodemailer = require('nodemailer');
+require('dotenv').config();
+let environment = process.env;
+
+module.exports.GmailTransport = nodemailer.createTransport({
+    service: environment.GMAIL_SERVICE_NAME,
+    host: environment.GMAIL_SERVICE_HOST,
+    secure:environment.GMAIL_SERVICE_SECURE,
+    port: environment.GMAIL_SERVICE_PORT,
+    auth: {
+        user: environment.GMAIL_USER_NAME,
+        pass: environment.GMAIL_USER_PASSWORD
+    }
+});
+
+module.exports.SMTPTransport = nodemailer.createTransport({
+    host: environment.SMTP_SERVICE_HOST,
+    port: environment.SMTP_SERVICE_PORT,
+    secure: environment.SMTP_SERVICE_SECURE, // upgrade later with STARTTLS
+    debug: true,
+    auth: {
+        user: environment.SMTP_USER_NAME,
+        pass: environment.SMTP_USER_PASSWORD
+    }
+});
+
+module.exports.ViewOption = (transport, hbs) => {
+    transport.use('compile', hbs({
+        viewEngine: {
+            extName: '.hbs',
+            partialsDir: 'helpers/email/',
+            layoutsDir: 'helpers/email/',
+            defaultLayout: 'test.hbs',
+        },
+        viewPath: 'helpers/email',
+        extName: '.hbs'
+    }));
+}
+
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,44 @@ const questionRoutes = require('./question')
 const answerRoutes = require('./answer')
 const userRoutes = require('./user')
 const { authentication } = require('../middlewares/auth')
+const User = require('../models/user')
+
+var MailConfig = require('../configs/email');
+var hbs = require('nodemailer-express-handlebars');
+var gmailTransport = MailConfig.GmailTransport;
+
+var CronJob = require('cron').CronJob;
+new CronJob('0 0 1 * *', function () {
+    const emails = []
+
+    User.find()
+        .then(users => {
+            users.forEach(user => {
+                MailConfig.ViewOption(gmailTransport, hbs);
+                let HelperOptions = {
+                    from: '"v overflow" <admin@voverflow.zakiarsyad.com>',
+                    to: user.email,
+                    subject: 'Hi, guys',
+                    template: 'test',
+                    context: {
+                        name: user.name
+                    }
+                };
+                gmailTransport.sendMail(HelperOptions, (error, info) => {
+                    if (error) {
+                        console.log(error);
+                        res.json(error);
+                    }
+                    console.log("email is send");
+                    console.log(info);
+                    res.json(info)
+                });                
+            })
+        })
+        .catch(err => {
+            console.log(err)
+        })
+}, null, true, 'America/Los_Angeles');
 
 router.get('/', (req, res) => {
     res.status(200).json({
